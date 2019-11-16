a7104a6cf818536be60297a3335811471cf2a04e
Gleb Korolkov <gkorolkov@loyalty.com>
2019-11-16 14:09:51 -0500

    Expand setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='datagen',
-    version='1.0',
-    packages=find_packages()
+    name="datagen",
+    version="1.0",
+    author="Gleb Korolkov",
+    author_email="gkorolkov@loyalty.com",
+    description="Fake data generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5"
 )
